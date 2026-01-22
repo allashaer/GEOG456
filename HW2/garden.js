4bf46e5d7cd96477980a3a70326953b9c8182d8f
allasha roth <allasha@unc.edu>
2026-01-21 23:08:02 -0500

    added files and updated hw2

---
+++ HW2/garden.js
@@ -0,0 +1,7 @@
+var garden={
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "descrip": "Oax botanical garden" }, "geometry": { "type": "Point", "coordinates": [ -96.722123, 17.065776 ] } }
+]
+}
