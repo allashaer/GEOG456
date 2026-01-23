fcd937ebdb048741c3fb74256413bdae7f7c4c61
allasha roth <allasha@unc.edu>
2026-01-22 22:52:05 -0500

    Updated coordinates and format

--- HW2/garden.js
+++ HW2/garden.js
@@ -2,6 +2,6 @@ var garden={
 "type": "FeatureCollection",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
 "features": [
-{ "type": "Feature", "properties": { "id": 1, "descrip": "Oax botanical garden" }, "geometry": { "type": "Point", "coordinates": [ -96.722123, 17.065776 ] } }
+{ "type": "Feature", "properties": { "id": 1, "descrip": "Oax botanical garden" }, "geometry": { "type": "Point", "coordinates": [ -96.722358, 17.067007 ] } }
 ]
 }
