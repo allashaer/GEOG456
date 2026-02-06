56932e7de67447510d70293898666c4f3cb57dd6
allasha roth <allasha@unc.edu>
2026-02-05 21:14:41 -0500

    HW3

---
+++ HW3/observations-677386.js
@@ -0,0 +1,471 @@
+var fowl = {
+   "type": "FeatureCollection",
+   "name": "bird",
+   "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+   "features": [
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -78.8817859814,35.8556113942 ]
+    },
+    "properties": {
+    "id":333063189,
+    "observed_on":"2026-01-01",
+    "species_guess":"Bufflehead",
+    "scientific_name":"Bucephala albeola",
+    "common_name":"Bufflehead",
+    "iconic_taxon_name":"Aves",
+    "taxon_id":6993
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -78.8817859814,35.8556113942 ]
+    },
+    "properties": {
+    "id":333063250,
+    "observed_on":"2026-01-01",
+    "species_guess":"Bufflehead",
+    "scientific_name":"Bucephala albeola",
+    "common_name":"Bufflehead",
+    "iconic_taxon_name":"Aves",
+    "taxon_id":6993
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -78.8817859814,35.8556113942 ]
+    },
+    "properties": {
+    "id":333072703,
+    "observed_on":"2026-01-01",
+    "species_guess":"Gadwall",
+    "scientific_name":"Mareca strepera",
+    "common_name":"Gadwall",
+    "iconic_taxon_name":"Aves",
+    "taxon_id":558439
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -78.8817859814,35.8556113942 ]
+    },
+    "properties": {
+    "id":333072991,
+    "observed_on":"2026-01-01",
+    "species_guess":"Ruddy Duck",
+    "scientific_name":"Oxyura jamaicensis",
+    "common_name":"Ruddy Duck",
+    "iconic_taxon_name":"Aves",
+    "taxon_id":850859
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -78.8817859814,35.8556113942 ]
+    },
+    "properties": {
+    "id":333073499,
+    "observed_on":"2026-01-01",
+    "species_guess":"Gadwall",
+    "scientific_name":"Mareca strepera",
+    "common_name":"Gadwall",
+    "iconic_taxon_name":"Aves",
+    "taxon_id":558439
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -78.9551783333,35.9037633333 ]
+    },
+    "properties": {
+    "id":333207402,
+    "observed_on":"2026-01-02",
+    "species_guess":"Canada Goose",
+    "scientific_name":"Branta canadensis",
+    "common_name":"Canada Goose",
+    "iconic_taxon_name":"Aves",
+    "taxon_id":7089
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -78.9704083333,35.9737066667 ]
+    },
+    "properties": {
+    "id":333466053,
+    "observed_on":"2026-01-02",
+    "species_guess":"Hooded Merganser",
+    "scientific_name":"Lophodytes cucullatus",
+    "common_name":"Hooded Merganser",
+    "iconic_taxon_name":"Aves",
+    "taxon_id":7109
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -78.8967365293,36.0320358991 ]
+    },
+    "properties": {
+    "id":333764015,
+    "observed_on":"2026-01-03",
+    "species_guess":"Mallard",
+    "scientific_name":"Anas platyrhynchos",
+    "common_name":"Mallard",
+    "iconic_taxon_name":"Aves",
+    "taxon_id":6930
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -78.896718731,36.0322520449 ]
+    },
+    "properties": {
+    "id":333764087,
+    "observed_on":"2026-01-04",
+    "species_guess":"Mallard",
+    "scientific_name":"Anas platyrhynchos",
+    "common_name":"Mallard",
+    "iconic_taxon_name":"Aves",
+    "taxon_id":6930
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -78.87368,35.8843333333 ]
+    },
+    "properties": {
+    "id":333985474,
+    "observed_on":"2026-01-08",
+    "species_guess":"Mallard",
+    "scientific_name":"Anas platyrhynchos",
+    "common_name":"Mallard",
+    "iconic_taxon_name":"Aves",
+    "taxon_id":6930
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -78.9383166667,35.9141116667 ]
+    },
+    "properties": {
+    "id":334091912,
+    "observed_on":"2026-01-09",
+    "species_guess":"Hooded Merganser",
+    "scientific_name":"Lophodytes cucullatus",
+    "common_name":"Hooded Merganser",
+    "iconic_taxon_name":"Aves",
+    "taxon_id":7109
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -79.0061416667,35.904065 ]
+    },
+    "properties": {
+    "id":334264205,
+    "observed_on":"2026-01-10",
+    "species_guess":"Mallard",
+    "scientific_name":"Anas platyrhynchos",
+    "common_name":"Mallard",
+    "iconic_taxon_name":"Aves",
+    "taxon_id":6930
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -78.9674841816,35.9685155197 ]
+    },
+    "properties": {
+    "id":335369023,
+    "observed_on":"2026-01-19",
+    "species_guess":"Canada Goose",
+    "scientific_name":"Branta canadensis",
+    "common_name":"Canada Goose",
+    "iconic_taxon_name":"Aves",
+    "taxon_id":7089
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -78.967505,35.9684783333 ]
+    },
+    "properties": {
+    "id":335369125,
+    "observed_on":"2026-01-19",
+    "species_guess":"Canada Goose",
+    "scientific_name":"Branta canadensis",
+    "common_name":"Canada Goose",
+    "iconic_taxon_name":"Aves",
+    "taxon_id":7089
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -78.9673311184,35.9685478227 ]
+    },
+    "properties": {
+    "id":335369192,
+    "observed_on":"2026-01-19",
+    "species_guess":"Mallard",
+    "scientific_name":"Anas platyrhynchos",
+    "common_name":"Mallard",
+    "iconic_taxon_name":"Aves",
+    "taxon_id":6930
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -78.8122166667,35.95993 ]
+    },
+    "properties": {
+    "id":335371408,
+    "observed_on":"2026-01-12",
+    "species_guess":"Canada Goose",
+    "scientific_name":"Branta canadensis",
+    "common_name":"Canada Goose",
+    "iconic_taxon_name":"Aves",
+    "taxon_id":7089
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -79.0069277833,35.9055533333 ]
+    },
+    "properties": {
+    "id":335375135,
+    "observed_on":"2026-01-19",
+    "species_guess":"Canada Goose",
+    "scientific_name":"Branta canadensis",
+    "common_name":"Canada Goose",
+    "iconic_taxon_name":"Aves",
+    "taxon_id":7089
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -78.9647636593,35.9770122387 ]
+    },
+    "properties": {
+    "id":335376306,
+    "observed_on":"2026-01-19",
+    "species_guess":"Canada Goose",
+    "scientific_name":"Branta canadensis",
+    "common_name":"Canada Goose",
+    "iconic_taxon_name":"Aves",
+    "taxon_id":7089
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -79.0158279665,35.937445097 ]
+    },
+    "properties": {
+    "id":335381445,
+    "observed_on":"2026-01-19",
+    "species_guess":"Mallard",
+    "scientific_name":"Anas platyrhynchos",
+    "common_name":"Mallard",
+    "iconic_taxon_name":"Aves",
+    "taxon_id":6930
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -78.9674264236,35.9685304532 ]
+    },
+    "properties": {
+    "id":335389833,
+    "observed_on":"2026-01-19",
+    "species_guess":"Mallard",
+    "scientific_name":"Anas platyrhynchos",
+    "common_name":"Mallard",
+    "iconic_taxon_name":"Aves",
+    "taxon_id":6930
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -78.9646616667,35.977355 ]
+    },
+    "properties": {
+    "id":335389879,
+    "observed_on":"2026-01-19",
+    "species_guess":"Canada Goose",
+    "scientific_name":"Branta canadensis",
+    "common_name":"Canada Goose",
+    "iconic_taxon_name":"Aves",
+    "taxon_id":7089
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -78.941435253,35.8943357031 ]
+    },
+    "properties": {
+    "id":335510497,
+    "observed_on":"2026-01-13",
+    "species_guess":"Canada Goose",
+    "scientific_name":"Branta canadensis",
+    "common_name":"Canada Goose",
+    "iconic_taxon_name":"Aves",
+    "taxon_id":7089
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -78.9488100602,35.9081874436 ]
+    },
+    "properties": {
+    "id":335511191,
+    "observed_on":"2026-01-13",
+    "species_guess":"Mallard",
+    "scientific_name":"Anas platyrhynchos",
+    "common_name":"Mallard",
+    "iconic_taxon_name":"Aves",
+    "taxon_id":6930
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -79.005775,35.9058266667 ]
+    },
+    "properties": {
+    "id":336124178,
+    "observed_on":"2026-01-25",
+    "species_guess":"Hooded Merganser",
+    "scientific_name":"Lophodytes cucullatus",
+    "common_name":"Hooded Merganser",
+    "iconic_taxon_name":"Aves",
+    "taxon_id":7109
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -78.8997000077,35.9588410223 ]
+    },
+    "properties": {
+    "id":336140748,
+    "observed_on":"2026-01-25",
+    "species_guess":"Bufflehead",
+    "scientific_name":"Bucephala albeola",
+    "common_name":"Bufflehead",
+    "iconic_taxon_name":"Aves",
+    "taxon_id":6993
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -78.9850871265,35.8715842443 ]
+    },
+    "properties": {
+    "id":336140860,
+    "observed_on":"2026-01-16",
+    "species_guess":"Mallard",
+    "scientific_name":"Anas platyrhynchos",
+    "common_name":"Mallard",
+    "iconic_taxon_name":"Aves",
+    "taxon_id":6930
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -78.8835355,36.0017055 ]
+    },
+    "properties": {
+    "id":336267357,
+    "observed_on":"2026-01-19",
+    "species_guess":"Canada Goose",
+    "scientific_name":"Branta canadensis",
+    "common_name":"Canada Goose",
+    "iconic_taxon_name":"Aves",
+    "taxon_id":7089
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -78.9120388889,35.8903555 ]
+    },
+    "properties": {
+    "id":336904058,
+    "observed_on":"2026-01-31",
+    "species_guess":"Hooded Merganser",
+    "scientific_name":"Lophodytes cucullatus",
+    "common_name":"Hooded Merganser",
+    "iconic_taxon_name":"Aves",
+    "taxon_id":7109
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -78.9123229894,35.8864141924 ]
+    },
+    "properties": {
+    "id":336904059,
+    "observed_on":"2026-01-31",
+    "species_guess":"Redhead",
+    "scientific_name":"Aythya americana",
+    "common_name":"Redhead",
+    "iconic_taxon_name":"Aves",
+    "taxon_id":7056
+    }
+  }
+]
+}
